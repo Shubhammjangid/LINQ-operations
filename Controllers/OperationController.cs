2777c210717489b20bb4426be941387e60054c59
Shubhammjangid <shubham.jangid@netsolutions.com>
2022-02-08 12:34:02 +0530

    Initial commit

---
+++ Controllers/OperationController.cs
@@ -0,0 +1,93 @@
+﻿using LINQ.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace LINQ.Controllers
+{
+    public class OperationController : Controller
+    {
+       
+        EmployeeStructure employee = new EmployeeStructure();
+        public ActionResult Index()
+        {
+            var employeeInfo = from x in employee.GetEmployeeList() select x;
+            return View(employeeInfo);
+        }
+
+        
+        public ActionResult Details(int id)
+        {
+            var getEmployeeDetail = employee.GetEmployeeList().Single(x => x.EmployeeID == id);
+            return View(getEmployeeDetail);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+   
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employee collection)
+        {
+            try
+            {
+              
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+     
+        public ActionResult Edit(int id)
+        {
+            var getEmployeeDetail = employee.GetEmployeeList().Single(x => x.EmployeeID == id);
+            return View(getEmployeeDetail);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Employee collection)
+        {
+            try
+            {
+                Employee updateList = employee.GetEmployeeList().Single(x => x.EmployeeID == id);
+                updateList.EmployeeID = collection.EmployeeID;
+                updateList.Name = collection.Name;
+                updateList.Department = collection.Department;
+                updateList.Age = collection.Age;
+                return RedirectToAction("Index", "Operation");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+
+namespace LINQ.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        public int EmployeeID { get; set; } 
+
+        public int Age { get; set; }    
+
+        public string Department { get; set; }
+
+
+    }
+
+    public class EmployeeStructure
+    {
+        public List<Employee> GetEmployeeList()
+        {
+            List<Employee> returnList = new List<Employee>();
+
+            Employee emp1 = new Employee();
+
+            Employee emp2 = new Employee();
+
+            Employee emp3 = new Employee();
+
+            Employee emp4 = new Employee();
+
+            Employee emp5 = new Employee();
+
+            Employee emp6 = new Employee();
+
+            emp1.Name = "Shubham Jangid";
+            emp1.Age = 21;
+            emp1.EmployeeID = 1;
+            emp1.Department = "Dotnet";
+
+            emp2.Name = "Abhijeet Desai";
+            emp2.Age = 21;
+            emp2.EmployeeID = 2;
+            emp2.Department = "React";
+
+            emp3.Name = "Karan gadekar";
+            emp3.Age = 31;
+            emp3.EmployeeID = 3;
+            emp3.Department = "Python";
+
+            emp4.Name = "Rohan Koli";
+            emp4.Age = 24;
+            emp4.EmployeeID = 4;
+            emp4.Department = "QC";
+
+            emp5.Name = "Prathamesh Chaughule";
+            emp5.Age = 24;
+            emp5.EmployeeID = 5;
+            emp5.Department = "PhP";
+
+            emp6.Name = "Sakshi Patil";
+            emp6.Age = 28;
+            emp6.EmployeeID = 6;
+            emp6.Department = "HR";
+
+            returnList.Add(emp1);
+            returnList.Add(emp2);
+            returnList.Add(emp3);
+            returnList.Add(emp4);
+            returnList.Add(emp5);
+            returnList.Add(emp6);
+
+            return returnList;
+
+
+        }
+    }
+    
+}
